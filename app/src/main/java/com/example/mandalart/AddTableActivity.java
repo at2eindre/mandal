3d0d0603c04ff9d25db917ad51063a0c9a749a83
at2eindre <ekekdmsdms2323@naver.com>
2022-09-25 23:29:02 +0900

    컬러피커가 날 울린다

--- app/src/main/java/com/example/mandalart/AddTableActivity.java
+++ app/src/main/java/com/example/mandalart/AddTableActivity.java
@@ -2,13 +2,19 @@ package com.example.mandalart;
 
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class AddTableActivity extends AppCompatActivity {
 
     DBHelper dbHelper;
     SQLiteDatabase sqLiteDatabase;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +24,50 @@ public class AddTableActivity extends AppCompatActivity {
         dbHelper = new DBHelper(this);
         sqLiteDatabase = dbHelper.getWritableDatabase();
 
+        button = (Button) findViewById(R.id.color_picker);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openColorPicker();
+            }
+        });
 
 
+    }
+    public void openColorPicker() {
+        final ColorPicker colorPicker = new ColorPicker(this);  // ColorPicker 객체 생성
+        ArrayList<String> colors = new ArrayList<>();  // Color 넣어줄 list
+
+        colors.add("#e8472e");
+        colors.add("#ff8c8c");
+        colors.add("#f77f23");
+        colors.add("#e38436");
+        colors.add("#f0e090");
+        colors.add("#98b84d");
+        colors.add("#a3d17b");
+        colors.add("#6ecf69");
+        colors.add("#6ec48c");
+        colors.add("#5aa392");
+        colors.add("#4b8bbf");
+        colors.add("#6a88d4");
+        colors.add("#736dc9");
+        colors.add("#864fbd");
+        colors.add("#d169ca");
+
+        colorPicker.setColors(colors)  // 만들어둔 list 적용
+                .setColumns(5)  // 5열로 설정
+                .setRoundColorButton(true)  // 원형 버튼으로 설정
+                .setOnChooseColorListener(new ColorPicker.OnChooseColorListener() {
+                    @Override
+                    public void onChooseColor(int position, int color) {
+//                        layout.setBackgroundColor(color);  // OK 버튼 클릭 시 이벤트
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // Cancel 버튼 클릭 시 이벤트
+                    }
+                }).show();  // dialog 생성
     }
 }
