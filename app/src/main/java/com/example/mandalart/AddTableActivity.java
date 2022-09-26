1a72014e0fa086e20b4a4abac3d6f967fd02318b
at2eindre <ekekdmsdms2323@naver.com>
2022-09-27 02:20:03 +0900

    이것저것 수정 및 테이블추가 창 구현중인데
    낼 아침에 계속해야겠다
    결국 컬러피커 오류는 못잡아서 다시 시작함 ㅋ,,

--- app/src/main/java/com/example/mandalart/AddTableActivity.java
+++ app/src/main/java/com/example/mandalart/AddTableActivity.java
@@ -2,14 +2,8 @@ package com.example.mandalart;
 
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.ArrayList;
-import petrov.kristiyan.colorpicker.ColorPicker;
 
 public class AddTableActivity extends AppCompatActivity {
 
