3b9f82d6a2def2f99fbaea136e35cce958e4d24b
JooHyeonJeong <jhj2000421@naver.com>
2022-10-10 03:50:36 +0900

    Joo
    입출력 일단 보이는거랑 기간은 해뒀삼!

--- app/src/main/java/com/example/mandalart/AddTableActivity.java
+++ app/src/main/java/com/example/mandalart/AddTableActivity.java
@@ -45,7 +45,7 @@ public class AddTableActivity extends AppCompatActivity {
 
     ArrayList<String> topicId = new ArrayList<>();
 
-    ArrayList<String> planId = new ArrayList<>();
+    ArrayList<String>[] planId = new ArrayList[9];
 
     static final int MAIN_MODE = 0;
     static final int SUB_MODE = 1;
@@ -62,6 +62,7 @@ public class AddTableActivity extends AppCompatActivity {
 
     int currentMode = MAIN_MODE;
     int insertWhere = SSUB1;
+    int subWhere = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,6 +77,8 @@ public class AddTableActivity extends AppCompatActivity {
 
         topicId.add(NULL);
         for(int i = 1; i <= COUNT; i++){
+            planId[i] = new ArrayList<String>();
+            planId[i].add(NULL);
             String tmpTopicId = "";
             while(true) {
                 tmpTopicId = setTopicId();
@@ -83,7 +86,29 @@ public class AddTableActivity extends AppCompatActivity {
             }
             topicId.add(tmpTopicId);
         }
-        insertTopicId(topicId);
+        insertTopicId();
+
+        for(int i = 1; i <= COUNT; i++){
+            for(int j = 1; j <= COUNT; j++){
+                String tmpPlanId = "";
+                while(true) {
+                    boolean flag = true;
+                    tmpPlanId = setPlanId();
+                    for(int k = 1; k <= COUNT ; k++) {
+                        if ((planId[k].contains(tmpPlanId))){
+
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if(flag) {
+                        planId[i].add(tmpPlanId);
+                        break;
+                    }
+                }
+            }
+        }
+        insertPlanId();
 
         layoutInflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         frameLayout = findViewById(R.id.insert_mandalart_table_framelayout);
@@ -105,7 +130,6 @@ public class AddTableActivity extends AppCompatActivity {
         main_theme.setText(theme);
 
         String subSelect = "SELECT * FROM " + dbHelper.TABLE_SUB + " WHERE " + dbHelper.ID + " = '"  + id + "';";
-        Log.i(LOG, subSelect);
         Cursor subCursor = sqLiteDatabase.rawQuery(subSelect, null);
         if(subCursor.moveToNext()){
             for(int i = 1; i <= COUNT; i++) {
@@ -146,7 +170,7 @@ public class AddTableActivity extends AppCompatActivity {
         sqLiteDatabase.execSQL(insertTableId);
     }
 
-    public void insertTopicId(ArrayList arrayList) {
+    public void insertTopicId() {
         String insertSub = "INSERT INTO " + DBHelper.TABLE_SUB + "(" + DBHelper.ID + ", " +
                 DBHelper.TOPIC_ID_1 + ", " + DBHelper.TOPIC_ID_2 + ", " + DBHelper.TOPIC_ID_3 + ", " +
                 DBHelper.TOPIC_ID_4 + ", " + DBHelper.TOPIC_ID_5 + ", " + DBHelper.TOPIC_ID_6 + ", " +
@@ -163,15 +187,44 @@ public class AddTableActivity extends AppCompatActivity {
         }
     }
 
+    public void insertPlanId(){
+        for(int i = 1; i<= COUNT ; i++) {
+            String insertSsub = "INSERT INTO " + DBHelper.TABLE_SSUB + "(" + DBHelper.TOPIC_ID + ", " +
+                    DBHelper.PLAN_ID_1 + ", " + DBHelper.PLAN_ID_2 + ", " + DBHelper.PLAN_ID_3 + ", " +
+                    DBHelper.PLAN_ID_4 + ", " + DBHelper.PLAN_ID_5 + ", " + DBHelper.PLAN_ID_6 + ", " +
+                    DBHelper.PLAN_ID_7 + ", " + DBHelper.PLAN_ID_8 + ") VALUES (" + topicId.get(i) + ", " +
+                    planId[i].get(SSUB1) + ", " + planId[i].get(SSUB2) + ", " + planId[i].get(SSUB3) + ", " +
+                    planId[i].get(SSUB4) + ", " + planId[i].get(SSUB5) + ", " + planId[i].get(SSUB6) + ", " +
+                    planId[i].get(SSUB7) + ", " + planId[i].get(SSUB8) + ")";
+            sqLiteDatabase.execSQL(insertSsub);
+
+        }
+
+        for(int i = 1; i <= COUNT; i++){
+            for(int j = 1; j <= COUNT; j++) {
+                String insertPlans = "INSERT INTO " + DBHelper.TABLE_PLANS + "(" + DBHelper.PLAN_ID + ", " +
+                        DBHelper.PLAN_NAME + ", " + DBHelper.PLAN_TERM + ", " + DBHelper.COMPLETE + ") VALUES (" + planId[i].get(j) +
+                        ", " + NULL +  ", " + NULL + ", " + NULL + ")";
+                sqLiteDatabase.execSQL(insertPlans);
+            }
+        }
+    }
+
+    public void mainThemeUpdate(){
+        String updateMain = "UPDATE " + DBHelper.TABLE_MAIN + " SET " + DBHelper.THEME+ " = '" + main_theme.getText() + "'" +
+                " WHERE " + DBHelper.ID + " = '" + tableId + "'";
+        sqLiteDatabase.execSQL(updateMain);
+    }
+
     public void topicsUpdate(int insertWhere){
         String updateTopics = "UPDATE " + DBHelper.TABLE_TOPICS + " SET " + DBHelper.TOPIC+ " = '" + sub[insertWhere].getText() + "'" +
                 " WHERE " + DBHelper.TOPIC_ID + " = '" + topicId.get(insertWhere) + "'";
         sqLiteDatabase.execSQL(updateTopics);
     }
 
-    public void plansUpdate(int insertWhere){
+    public void plansUpdate(int insertWhere, int subWhere){
         String updatePlans = "UPDATE " + DBHelper.TABLE_PLANS + " SET " + DBHelper.PLAN_NAME+ " = '" + ssub[insertWhere].getText() + "'" + ", " +
-                DBHelper.PLAN_TERM + " = " + "'기간'" + ", " + " WHERE " + DBHelper.PLAN_ID + " = '" + "planid" + "'";
+                DBHelper.PLAN_TERM + " = " + "'기간'" + " WHERE " + DBHelper.PLAN_ID + " = '" + planId[subWhere].get(insertWhere) + "'";
         sqLiteDatabase.execSQL(updatePlans);
     }
 
@@ -207,7 +260,7 @@ public class AddTableActivity extends AppCompatActivity {
         main_theme.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                savePrev(insertWhere);
+                savePrev(insertWhere, subWhere);
 
                 insertWhere=0;
                 editText.setText(main_theme.getText());
@@ -218,8 +271,8 @@ public class AddTableActivity extends AppCompatActivity {
             sub[i].setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    savePrev(insertWhere);
-
+                    savePrev(insertWhere, subWhere);
+                    subWhere = finalI;
                     insertWhere=finalI;
                     editText.setText(sub[finalI].getText());
                 }
@@ -230,7 +283,7 @@ public class AddTableActivity extends AppCompatActivity {
             button_sub[i].setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    savePrev(insertWhere);
+                    savePrev(insertWhere, subWhere);
                     resetDay(1);
 
                     changeView(finalI);
@@ -364,7 +417,7 @@ public class AddTableActivity extends AppCompatActivity {
         sub_topic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                savePrev(insertWhere);
+                savePrev(insertWhere, subWhere);
 
                 changeBackView();
             }
@@ -381,7 +434,7 @@ public class AddTableActivity extends AppCompatActivity {
             ssub[i].setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    savePrev(insertWhere);
+                    savePrev(insertWhere, subWhere);
                     resetDay(finalI);
 
                     insertWhere = finalI;
@@ -404,20 +457,26 @@ public class AddTableActivity extends AppCompatActivity {
         sun.setPaintFlags(sun.getPaintFlags()&~Paint.UNDERLINE_TEXT_FLAG);
     }
 
-    void savePrev(int insertWhere){
+    void savePrev(int insertWhere, int subWhere){
         if(currentMode == MAIN_MODE){
             //MAIN_MODE, insertWhere, editText.getText 입력
             //insertWhere=0이면 MAIN_THEME에 저장하면 된다!
 
-            if(insertWhere>0)sub[insertWhere].setText(editText.getText());
-            else main_theme.setText(editText.getText());
-            topicsUpdate(insertWhere);
+            if(insertWhere>0){
+                sub[insertWhere].setText(editText.getText());
+                topicsUpdate(insertWhere);
+
+            }
+            else {
+                main_theme.setText(editText.getText());
+                mainThemeUpdate();
+            }
         }
         else if(currentMode == SUB_MODE){
             //SUB_MODE, insertWhere, editText.getText, 날짜입력처리해서 입력
 
             ssub[insertWhere].setText(editText.getText());
-            plansUpdate(insertWhere);
+            plansUpdate(insertWhere, subWhere);
         }
     }
 
@@ -429,7 +488,7 @@ public class AddTableActivity extends AppCompatActivity {
     }
 
     void changeBackView(){
-        savePrev(insertWhere);
+        savePrev(insertWhere, subWhere);
 
         insertWhere = SSUB1;
         currentMode = MAIN_MODE;
@@ -476,6 +535,15 @@ public class AddTableActivity extends AppCompatActivity {
         return id;
     }
 
+    public String setPlanId(){
+        String id = "";
+        while(true){
+            id = makeId();
+            if(chkDupPlanId(id)) break;
+        }
+        return id;
+    }
+
     public String makeId(){
         int intTableId = (int) (Math.random() * 100000000);
         String StrTableId = Integer.toString(intTableId);
@@ -493,6 +561,13 @@ public class AddTableActivity extends AppCompatActivity {
         String topicSelect = "SELECT * FROM " + dbHelper.TABLE_TOPICS + " WHERE " + dbHelper.TOPIC_ID + " = '"  + id + "';";
         Cursor topicCursor = sqLiteDatabase.rawQuery(topicSelect, null);
         if(topicCursor.moveToNext()) return false;
+        return true;
+    }
+
+    public boolean chkDupPlanId(String id){
+        String planSelect = "SELECT * FROM " + dbHelper.TABLE_PLANS + " WHERE " + dbHelper.PLAN_ID + " = '"  + id + "';";
+        Cursor planCursor = sqLiteDatabase.rawQuery(planSelect, null);
+        if(planCursor.moveToNext()) return false;
         return true;
     }
 
